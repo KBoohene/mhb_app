aedf874bb7c3573dbff946dd3d02b8971bfa4c7b
Kwabena Boohene <okwabse@gmail.com>
2018-03-28 14:52:29 +0000

    Moved mhb db file

--- src/api/db_functions.js
+++ src/api/db_functions.js
@@ -1,13 +1,13 @@
 const SQLite = require('react-native-sqlite-storage')
 
-const db = SQLite.openDatabase({name : "mhb_db", createFromLocation : "~/data/mhb.db"});
+const db = SQLite.openDatabase({name : "mhb_db", createFromLocation : "~/mhb.db"});
 
 
 const functions = {
   getHymn(){
     db.transaction((tx) => {
-      tx.executeSql('SELECT * FROM hymns', [], (tx, results) => {
-          // Get rows with Web SQL Database spec compliance.
+      tx.executeSql('CREATE TABLE IF NOT EXISTS category  (ID Integer PRIMARY KEY AUTOINCREMENT,NAME varchar(25))', [], (tx, results) => {
+          console.log(results)
           
         });
     });
