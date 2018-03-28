8d1526b6474a45a3a1859c8a5aabdfdff74376da
Kwabena Boohene <okwabse@gmail.com>
2018-03-28 18:26:17 +0000

    Enabled passing data between pages

--- src/spa/Home.js
+++ src/spa/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View} from 'react-native';
 import Button from 'antd-mobile/lib/button';
+import SearchBar from 'antd-mobile/lib/search-bar'
 import db from '../api/db_functions'
 
 class Home extends Component{
@@ -8,23 +9,39 @@ class Home extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      lyrics: ''
-
+      num: '',
+      lyrics:''
     };  
 
   }
   static navigationOptions = { header: null }
   findHymn = () =>{
-    db.getHymn(1,(hymnText)=>{
+    db.getHymn(this.state.num,(hymnText)=>{
       this.setState({lyrics:hymnText})
+      
+      this.props.navigation.navigate('Hymns', {
+        hymnNumber: this.state.num,
+        hymnText: this.state.lyrics,
+      });
     })
   }
+
+  onChange= (value) => {
+    this.setState({ num: value });
+  };
 
   render(){
     return(
       <View>
-        <Button onClick={this.findHymn}>Search</Button>
-        <Text>{this.state.lyrics}</Text>
+        <View></View>
+        <View>
+          <SearchBar 
+            onChange={this.onChange}
+            placeholder="Search" 
+            maxLength={8} />
+          <Button onClick={this.findHymn}>Search</Button>
+        </View>     
+        <Text>{this.state.num}</Text>
       </View>
       )
   }
--- src/spa/Hymn.js
+++ src/spa/Hymn.js
@@ -2,16 +2,38 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  ScrollView
 } from 'react-native';
+import SearchBar from 'antd-mobile/lib/search-bar';
 
 class Hymn extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      num: '',
+      lyrics:''
+    };  
+
+  }
 
   static navigationOptions = { header: null }
+
+  componentWillMount(){
+    let { params } = this.props.navigation.state;
+    let hymn_Number = params ? params.hymnNumber : null;
+    let hymn_lyrics = params ? params.hymnText : null;
+    this.setState({lyrics:hymn_lyrics})
+  }
+
   render(){
     return(
       <View>
-        <Text>Hymn Page</Text>
+        <View>
+          <SearchBar placeholder="Search" maxLength={8} />
+        </View>
+        <ScrollView>
+        </ScrollView>
       </View>
       )
   }
